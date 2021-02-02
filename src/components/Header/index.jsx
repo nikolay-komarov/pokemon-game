028663a7a202917c0de874a2b708f923aa1dedcc
nikolay-komarov <kny.komarov@gmail.com>
2021-02-02 09:06:16 +0300

    добавит страницы HomePage, GamePage и псевдороутинг

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import s from './style.module.css';
 
-const Header = ({title, descr}) => {
+const Header = ({title, descr, onClickButton}) => {
+  const handleClick = () => {
+    console.log('<Header />');
+    onClickButton && onClickButton('game');
+  };
+
   return (
     <header className={s.root}>
       <div className={s.forest}/>
       <div className={s.container}>
         {title && <h1>{title}</h1>}
         {descr && <p>{descr}</p>}
+        <button onClick={handleClick}>
+          StartGame
+        </button>
       </div>
     </header>
   );
---
+++ src/routes/Game/index.jsx
@@ -0,0 +1,10 @@
+
+const GamePage = () => {
+  return (
+    <div>
+      This is GamePage!
+    </div>
+  );
+};
+
+export default GamePage;
