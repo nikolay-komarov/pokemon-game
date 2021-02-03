594e34e43d3c0acd19424ca1ce9909ffd184943e
nikolay-komarov <kny.komarov@gmail.com>
2021-02-03 23:11:58 +0300

    добавит изменения в Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,6 +10,8 @@ const Header = ({title, descr}) => {
   return (
     <header className={s.root}>
       <div className={s.forest}/>
+      <div className={s.silhouette}/>
+      <div className={s.moon}/>
       <div className={s.container}>
         {title && <h1>{title}</h1>}
         {descr && <p>{descr}</p>}
