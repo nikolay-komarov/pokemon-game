8b4374fe471cea321c6eb8816898d36a1bc5f14a
nikolay-komarov <kny.komarov@gmail.com>
2021-02-09 22:38:03 +0300

    внесет изменения в PokemonCard

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -2,13 +2,13 @@ import cardBackSide from './assets/card-back-side.jpg';
 import s from './style.module.css';
 import cn from 'classnames';
 
-const PokemonCard = ({name, img, id, type, values, isActive, onPokemonCardClick}) => {
+const PokemonCard = ({name, img, id, type, values, isActive, minimize, className, onPokemonCardClick}) => {
   const handleClick = () => {
     onPokemonCardClick(id);
   };
   return (
     <div className={s.root} onClick={handleClick}>
-      <div className={cn(s.pokemonCard, {[s.active]: isActive})}>
+      <div className={cn(className, s.pokemonCard, {[s.active]: isActive})}>
         <div className={s.cardFront}>
           <div className={cn(s.wrap, s.front)}>
             <div className={cn(s.pokemon, s[type])}>
@@ -21,11 +21,14 @@ const PokemonCard = ({name, img, id, type, values, isActive, onPokemonCardClick}
               <div className={s.imgContainer}>
                 <img src={img} alt={name} />
               </div>
-              <div className={s.info}>
-                <span className={s.number}>#{id}</span>
-                <h3 className={s.name}>{name}</h3>
-                <small className={s[type]}>Type: <span>{type}</span></small>
-              </div>
+              {
+                !minimize &&
+                <div className={s.info}>
+                  <span className={s.number}>#{id}</span>
+                  <h3 className={s.name}>{name}</h3>
+                  <small className={s[type]}>Type: <span>{type}</span></small>
+                </div>
+              }
             </div>
           </div>
         </div>
