b3fd6da3eaaa58700bab43ee93b91f967a107529
nikolay-komarov <kny.komarov@gmail.com>
2021-02-14 16:57:36 +0300

    добавит запрос на получение данных от сервера и отображение карточек для второго игрока в BoardPage

--- src/routes/Game/routes/Board/index.jsx
+++ src/routes/Game/routes/Board/index.jsx
@@ -9,10 +9,11 @@ import s from './style.module.css';
 
 const BoardPage = () => {
   const [board, setBoard] = useState([]);
+  const [player2, setPlayer2] = useState([]);
   const {pokemons} = useContext(PokemonContext);
   const history = useHistory();
 
-  console.log(board);
+  console.log(player2);
 
   const handlePokemonCardClick = (id) => {
     console.log('click');
@@ -23,6 +24,10 @@ const BoardPage = () => {
     const boardRequest = await boardResponse.json();
 
     setBoard(boardRequest.data);
+
+    const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
+    const player2Request = await  player2Response.json();
+    setPlayer2(player2Request.data);
   }, []);
 
   // if (Object.keys(pokemons).length === 0) {
@@ -72,6 +77,25 @@ const BoardPage = () => {
               }
             </div>
           ))
+        }
+      </div>
+
+      <div className={s.playerTwo}>
+        {
+          player2.map((item) =>
+            <PokemonCard
+              key={item.key}
+              name={item.name}
+              img={item.img}
+              id={item.id}
+              type={item.type}
+              values={item.values}
+              isActive={true}
+              isSelected={item.selected}
+              classsName={s.card}
+              minimize={true}
+              onPokemonCardClick={handlePokemonCardClick}
+            />)
         }
       </div>
     </div>
