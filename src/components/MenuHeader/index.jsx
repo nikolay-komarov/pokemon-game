eae8cf583f512131076968e213b29dd1896c3e3e
nikolay-komarov <kny.komarov@gmail.com>
2021-02-28 22:58:01 +0300

    добавит модальное окно

--- src/components/MenuHeader/index.jsx
+++ src/components/MenuHeader/index.jsx
@@ -1,18 +1,18 @@
 import {useState} from 'react';
 import Menu from "../Menu";
 import Navbar from "../Navbar";
+import Modal from "../Modal";
 
 const MenuHeader = ({bgActive}) => {
   const [isOpen, setOpen] = useState(null);
-  const [isOpenModal, setOpenModal] = useState(null);
+  const [isOpenModal, setOpenModal] = useState(false);
 
   const handleClickHamburg = () => {
     setOpen(prevState => !prevState);
   };
   const handleClickLogin = () => {
     setOpenModal(prevState => !prevState);
-    console.log('login click');
-  }
+  };
 
   return (
     <>
@@ -26,6 +26,13 @@ const MenuHeader = ({bgActive}) => {
         onClickHamburg={handleClickHamburg}
         onClickLogin={handleClickLogin}
       />
+      <Modal
+        isOpen={isOpenModal}
+        title="Login..."
+        onCloseModal={handleClickLogin}
+      >
+        Some text
+      </Modal>
     </>
   );
 };
---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,42 @@
+import {useRef} from 'react';
+
+import s from './style.module.css';
+import cn from 'classnames';
+
+const Modal = ({isOpen, title, children, onCloseModal}) => {
+  const modalEl = useRef();
+
+  const handleCloseModal = () => {
+    onCloseModal && onCloseModal();
+  };
+  const handleClickRoot = (evt) => {
+    if (!modalEl.current.contains(evt.target)) {
+      handleCloseModal();
+    }
+  };
+
+  return (
+    <div
+      className={cn(s.root, {[s.open]: isOpen})}
+      onClick={handleClickRoot}
+    >
+      <div
+        ref={modalEl}
+        className={s.modal}
+      >
+        <div className={s.head}>
+          {title}
+          <span
+            className={s.btnClose}
+            onClick={handleCloseModal}
+          />
+        </div>
+        <div className={s.content}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
